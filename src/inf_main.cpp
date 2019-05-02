6842ea5ec16feabc28e2347d36237669bc382721
RickRichter <RickRichter>
2019-05-02 14:39:55 +0200

    Alles zum Index hinzufügen

---
+++ src/inf_main.cpp
@@ -0,0 +1,56 @@
+#include <ros_test/turtle_abstract.h>
+
+std::shared_ptr<tutorial::AbstractTurtle> turtle;
+
+void draw_u( bool cw ) {
+    int sign;
+    if ( cw ) {
+        sign = -1;
+    } else {
+        sign = 1;
+    }
+
+    turtle->turn( sign * 45 );		//. zu ->
+    for (int i = 0; i < 3; i++) {
+        turtle->forward(3);
+        turtle->turn( sign * 90 );
+    }
+    turtle->turn( sign * -45 );
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "inf");
+
+    bool use_arc = false;
+    ros::NodeHandle pnh("~");
+    pnh.param<bool>("use_arc", use_arc, false);
+
+    turtle = std::make_shared<tutorial::AbstractTurtle>();
+
+    if(use_arc){
+      turtle->turn(-45);
+      turtle->forward(1);
+      turtle->arc(1, 270);
+      turtle->forward(1);
+      turtle->turn(-90);
+      turtle->forward(1);
+      turtle->arc(1, 270);
+      turtle->forward(1);
+    }else{
+
+    // turtle should draw an infinty sign
+    turtle->turn(45);
+    turtle->forward( sqrt(4.5) );
+    draw_u( true );
+    turtle->forward( 2 * sqrt(4.5) );
+    draw_u( false );
+    turtle->forward( sqrt(4.5) );
+    }
+
+    turtle.reset();
+
+    ROS_INFO_STREAM("Finished");
+
+    return 0;
+}
